1382b43ac6b0354116335f08c49eec470e0c1379
Lucas Bordignon <lucaspbordignon99@gmail.com>
2016-10-26 14:19:18 -0200

    Generate a XML file with the trace output

--- src/test/java/SimulationRunner.java
+++ src/test/java/SimulationRunner.java
@@ -1,6 +1,7 @@
 import desmoj.core.simulator.Experiment;
 import desmoj.core.simulator.Model;
 import desmoj.core.simulator.TimeInstant;
+import desmoj.extensions.xml.report.XMLTraceOutput;
 import lapesd.saturnus.simulator.AbstractSimulator;
 
 public class SimulationRunner {
@@ -8,15 +9,21 @@ public class SimulationRunner {
     public static void main(String[] args) {
         Model model = new AbstractSimulator();
         Experiment exp = new Experiment("Simulation");
+        XMLTraceOutput xmlTrace = new XMLTraceOutput();
 
         // Connect both model and experiment.
         model.connectToExperiment(exp);
 
-        // Experiment parameters.
+        // Write the trace into a XML file
+        xmlTrace.open(null, "Simulation");
+        exp.addTraceReceiver(xmlTrace);
+
         exp.traceOn(new TimeInstant(0));
         exp.start();
 
         exp.report();
+        xmlTrace.close();
         exp.finish();
+
     }
 }
