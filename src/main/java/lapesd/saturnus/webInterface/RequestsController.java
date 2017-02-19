9182be2cb49dc5366c892ae6f1d2acbfafcfd204
Lucas Bordignon <lucaspbordignon99@gmail.com>
2017-02-19 12:19:09 -0300

    Parameters received through the interface

--- src/main/java/lapesd/saturnus/webInterface/RequestsController.java
+++ src/main/java/lapesd/saturnus/webInterface/RequestsController.java
@@ -1,8 +1,16 @@
 package lapesd.saturnus.webInterface;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class RequestsController {
 
+    @PostMapping("/start")
+    public String startSimulation(@RequestBody MultiValueMap<String, String> formData) {
+
+        return "simulation";
+    }
 }
