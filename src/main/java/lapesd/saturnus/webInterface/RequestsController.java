75dc48b239c62ab1de6cc6416eacbe1510a7135b
Lucas Bordignon <lucaspbordignon99@gmail.com>
2017-02-21 15:43:29 -0300

    Start button connected to the model. Old JavaFX files removed

--- src/main/java/lapesd/saturnus/webInterface/RequestsController.java
+++ src/main/java/lapesd/saturnus/webInterface/RequestsController.java
@@ -1,16 +1,20 @@
 package lapesd.saturnus.webInterface;
 
+import lapesd.saturnus.simulator.SimulationController;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.io.IOException;
+
 @Controller
 public class RequestsController {
-
+    
     @PostMapping("/start")
-    public String startSimulation(@RequestBody MultiValueMap<String, String> formData) {
-
+    public String startSimulation(@RequestBody MultiValueMap<String, String> formData) throws IOException{
+        // TODO: Check if the map is not empty;
+        SimulationController.initSimulation(formData.toSingleValueMap());
         return "simulation";
     }
 }
