7d481561df3d55bbccffa709df73fd4d3655f896
Lucas Bordignon <lucaspbordignon99@gmail.com>
2017-02-21 14:48:44 -0300

    Simulation controller adapted to receive the params in one map

--- src/main/java/lapesd/saturnus/simulator/SimulationController.java
+++ src/main/java/lapesd/saturnus/simulator/SimulationController.java
@@ -4,6 +4,7 @@ import com.opencsv.CSVWriter;
 import desmoj.core.simulator.Experiment;
 import desmoj.core.simulator.TimeInstant;
 import javafx.scene.control.TextArea;
+import org.apache.commons.collections.map.HashedMap;
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,8 +12,18 @@ import java.util.Map;
 
 public class SimulationController {
 
-    public static void initSimulation(TextArea output, Map parameters, String fType, String aPattern) throws IOException{
-        AbstractSimulator model = new AbstractSimulator(parameters, fType, aPattern);
+    public static void initSimulation(TextArea output, Map<String, String> parameters) throws IOException{
+        // Pre-processing the parameters
+        Map<String, Integer> numericParams = new HashedMap();
+        numericParams.put("numberTasks", Integer.parseInt(parameters.get("numberTasks")));
+        numericParams.put("numberSegments", Integer.parseInt(parameters.get("numberSegments")));
+        numericParams.put("numberDataNodes", Integer.parseInt(parameters.get("numberDataNodes")));
+        numericParams.put("blockSize", Integer.parseInt(parameters.get("blockSize")));
+        numericParams.put("requestSize", Integer.parseInt(parameters.get("requestSize")));
+        numericParams.put("stripeSize", Integer.parseInt(parameters.get("stripeSize")));
+        numericParams.put("stripeCount", Integer.parseInt(parameters.get("stripeCount")));
+
+        AbstractSimulator model = new AbstractSimulator(numericParams, parameters.get("fileType"), parameters.get("accessPattern"));
         Experiment experiment = new Experiment("Simulation");
         experiment.setSilent(true);
 
